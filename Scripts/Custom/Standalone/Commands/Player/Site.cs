0a416068f3f04412fa714bc084e1acef6964c0b8
Victor Nogueira <felladrin@gmail.com>
2015-12-03 19:22:39 -0300

    Added [Site command for players.

---
+++ Scripts/Custom/Standalone/Commands/Player/Site.cs
@@ -0,0 +1,22 @@
+//   ___|========================|___
+//   \  |  Written by Felladrin  |  /   Site Command - Current version: 1.0 (August 31, 2013)
+//    > |      August 2013       | <
+//   /__|========================|__\   Description: Opens the shard website.
+
+namespace Server.Commands
+{
+    public class SiteCommand
+    {
+        public static void Initialize()
+        {
+            CommandSystem.Register("Site", AccessLevel.Player, new CommandEventHandler(OpenWebSite));
+        }
+
+        [Usage("Site")]
+        [Description("Opens the shard website in your browser.")]
+        private static void OpenWebSite(CommandEventArgs e)
+        {
+            e.Mobile.LaunchBrowser("https://github.com/felladrin/last-wish");
+        }
+    }
+}
