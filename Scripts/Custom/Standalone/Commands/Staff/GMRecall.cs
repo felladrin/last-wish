279fdedf5a3d96a1dea1fb96ab072dc5b70a2795
Victor Nogueira <felladrin@gmail.com>
2015-12-06 17:05:57 -0300

    Added Staff Command: [GMRecall

---
+++ Scripts/Custom/Standalone/Commands/Staff/GMRecall.cs
@@ -0,0 +1,64 @@
+using Server;
+using Server.Network;
+using Server.Items;
+using Server.Mobiles;
+using Server.Misc;
+using Server.Targeting;
+using Server.Commands;
+
+namespace Server.Commands
+{
+    public class GMRecall
+    {
+        public static void Initialize()
+        {
+            CommandSystem.Register("GMRecall", AccessLevel.Counselor, new CommandEventHandler(GMRecall_OnCommand));
+        }
+
+        [Usage("GMRecall")]
+        [Description("Recall in a targeted rune.")]
+        private static void GMRecall_OnCommand(CommandEventArgs e)
+        {
+            e.Mobile.SendMessage("Select a default runebook.");
+            e.Mobile.Target = new RecallTarget(e.Mobile);
+        }
+
+        private class RecallTarget : Target
+        {
+            public RecallTarget(Mobile m) : base(1, false, TargetFlags.None) { }
+
+            protected override void OnTarget(Mobile from, object target)
+            {
+                if (target is RecallRune)
+                {
+                    RecallRune t = (RecallRune)target;
+
+                    if (t.Marked == true)
+                    {
+                        from.Location = t.Target;
+                        from.Map = t.TargetMap;
+                    }
+                    else
+                        from.SendLocalizedMessage(502354); // Target is not marked.
+                }
+                else
+                    if (target is Runebook)
+                    {
+                        RunebookEntry e = ((Runebook)target).Default;
+
+                        if (e != null)
+                        {
+                            from.Location = e.Location;
+                            from.Map = e.Map;
+                        }
+                        else
+                            from.SendLocalizedMessage(502354); // Target is not marked.
+                    }
+                    else
+                    {
+                        from.SendMessage("It cannot be done");
+                    }
+            }
+        }
+    }
+}
