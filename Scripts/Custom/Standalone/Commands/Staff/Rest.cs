d09edd113c741f643b7b78d6f1f5ac6688add288
Victor Nogueira <felladrin@gmail.com>
2015-12-06 17:08:00 -0300

    Added Staff Command: [Rest

---
+++ Scripts/Custom/Standalone/Commands/Staff/Rest.cs
@@ -0,0 +1,42 @@
+﻿//   ___|========================|___
+//   \  |  Written by Felladrin  |  /
+//    > |      January 2011      | <
+//   /__|========================|__\
+
+using Server.Targeting;
+
+namespace Server.Commands
+{
+    public class Rest
+    {
+        public static void Initialize()
+        {
+            CommandSystem.Register("Rest", AccessLevel.GameMaster, new CommandEventHandler(Restore_OnCommand));
+        }
+
+        [Usage("Rest")]
+        [Description("Completely restore a character (life, mana, stam, hunger, thirst).")]
+        public static void Restore_OnCommand(CommandEventArgs e)
+        {
+            e.Mobile.Target = new restTarget();
+        }
+
+        public class restTarget : Target
+        {
+            public restTarget() : base(-1, false, TargetFlags.None) { }
+
+            protected override void OnTarget(Mobile from, object targeted)
+            {
+                if (targeted is Mobile)
+                {
+                    Mobile targ = (Mobile)targeted;
+                    targ.Hits = targ.HitsMax;
+                    targ.Mana = targ.ManaMax;
+                    targ.Stam = targ.StamMax;
+                    targ.Hunger = 20;
+                    targ.Thirst = 20;
+                }
+            }
+        }
+    }
+}
