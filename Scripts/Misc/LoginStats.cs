11b0b5de4209e9b2e107283404d01251cc11b186
Adrian <agalassik@gmail.com>
2015-12-16 00:35:48 -0300

    Raised Skillscap
    Skillscap raised to 1200, individual skill cap raised to 120.

--- Scripts/Misc/LoginStats.cs
+++ Scripts/Misc/LoginStats.cs
@@ -24,6 +24,65 @@ namespace Server.Misc
 				userCount, userCount == 1 ? "" : "s",
 				itemCount, itemCount == 1 ? "" : "s",
 				mobileCount, mobileCount == 1 ? "" : "s" );
+				
+				m.Skills[SkillName.Alchemy].Cap = 120;
+				m.Skills[SkillName.Anatomy].Cap = 120;
+				m.Skills[SkillName.AnimalLore].Cap = 120;
+				m.Skills[SkillName.ItemID].Cap = 120;
+				m.Skills[SkillName.ArmsLore].Cap = 120;
+				m.Skills[SkillName.Parry].Cap = 120;
+				m.Skills[SkillName.Begging].Cap = 120;
+				m.Skills[SkillName.Blacksmith].Cap = 120;
+				m.Skills[SkillName.Fletching].Cap = 120;
+				m.Skills[SkillName.Peacemaking].Cap = 120;
+				m.Skills[SkillName.Camping].Cap = 120;
+				m.Skills[SkillName.Carpentry].Cap = 120;
+				m.Skills[SkillName.Cartography].Cap = 120;
+				m.Skills[SkillName.Cooking].Cap = 120;
+				m.Skills[SkillName.DetectHidden].Cap = 120;
+				m.Skills[SkillName.Discordance].Cap = 120;
+				m.Skills[SkillName.EvalInt].Cap = 120;
+				m.Skills[SkillName.Healing].Cap = 120;
+				m.Skills[SkillName.Fishing].Cap = 120;
+				m.Skills[SkillName.Forensics].Cap = 120;
+				m.Skills[SkillName.Herding].Cap = 120;
+				m.Skills[SkillName.Hiding].Cap = 120;
+				m.Skills[SkillName.Provocation].Cap = 120;
+				m.Skills[SkillName.Inscribe].Cap = 120;
+				m.Skills[SkillName.Lockpicking].Cap = 120;
+				m.Skills[SkillName.Magery].Cap = 120;
+				m.Skills[SkillName.MagicResist].Cap = 120;
+				m.Skills[SkillName.Tactics].Cap = 120;
+				m.Skills[SkillName.Snooping].Cap = 120;
+				m.Skills[SkillName.Musicianship].Cap = 120;
+				m.Skills[SkillName.Poisoning].Cap = 120;
+				m.Skills[SkillName.Archery].Cap = 120;
+				m.Skills[SkillName.SpiritSpeak].Cap = 120;
+				m.Skills[SkillName.Stealing].Cap = 120;
+				m.Skills[SkillName.Tailoring].Cap = 120;
+				m.Skills[SkillName.AnimalTaming].Cap = 120;
+				m.Skills[SkillName.TasteID].Cap = 120;
+				m.Skills[SkillName.Tinkering].Cap = 120;
+				m.Skills[SkillName.Tracking].Cap = 120;
+				m.Skills[SkillName.Veterinary].Cap = 120;
+				m.Skills[SkillName.Swords].Cap = 120;
+				m.Skills[SkillName.Macing].Cap = 120;
+				m.Skills[SkillName.Fencing].Cap = 120;
+				m.Skills[SkillName.Wrestling].Cap = 120;
+				m.Skills[SkillName.Lumberjacking].Cap = 120;
+				m.Skills[SkillName.Mining].Cap = 120;
+				m.Skills[SkillName.Meditation].Cap = 120;
+				m.Skills[SkillName.Stealth].Cap = 120;
+				m.Skills[SkillName.RemoveTrap].Cap = 120;
+				m.Skills[SkillName.Necromancy].Cap = 120;
+				m.Skills[SkillName.Focus].Cap = 120;
+				m.Skills[SkillName.Chivalry].Cap = 120;
+				m.Skills[SkillName.Bushido].Cap = 120;
+				m.Skills[SkillName.Ninjitsu].Cap = 120;
+				m.Skills[SkillName.Spellweaving].Cap = 120;
+				m.Skills[SkillName.Mysticism].Cap = 120;
+				m.Skills[SkillName.Imbuing].Cap = 120;
+				m.Skills[SkillName.Throwing].Cap = 120;
 		}
 	}
 }
