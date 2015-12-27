34a42616e8cbd972ba2a36e5d4d4839617559daa
Victor Nogueira <felladrin@gmail.com>
2015-12-27 01:32:37 -0300

    Removed Free Movement and Beneficial Restrictions from all maps. So no one can move over anyone else without taking stamina loss. And everybody can perform beneficial actions on criminals/murderers. But Harmful Actions are still restricted outside Felucca.

--- Scripts/Misc/MapDefinitions.cs
+++ Scripts/Misc/MapDefinitions.cs
@@ -13,11 +13,11 @@ namespace Server.Misc
 			 */
 
 			RegisterMap( 0, 0, 0, 7168, 4096, 4, "Felucca",		MapRules.FeluccaRules );
-			RegisterMap( 1, 1, 1, 7168, 4096, 0, "Trammel",		MapRules.TrammelRules );
-			RegisterMap( 2, 2, 2, 2304, 1600, 1, "Ilshenar",	MapRules.TrammelRules );
-			RegisterMap( 3, 3, 3, 2560, 2048, 1, "Malas",		MapRules.TrammelRules );
-			RegisterMap( 4, 4, 4, 1448, 1448, 1, "Tokuno",		MapRules.TrammelRules );
-			RegisterMap( 5, 5, 5, 1280, 4096, 1, "TerMur",		MapRules.TrammelRules );
+            RegisterMap( 1, 1, 1, 7168, 4096, 0, "Trammel",		MapRules.HarmfulRestrictions );
+            RegisterMap( 2, 2, 2, 2304, 1600, 1, "Ilshenar",	MapRules.HarmfulRestrictions );
+            RegisterMap( 3, 3, 3, 2560, 2048, 1, "Malas",		MapRules.HarmfulRestrictions );
+            RegisterMap( 4, 4, 4, 1448, 1448, 1, "Tokuno",		MapRules.HarmfulRestrictions );
+            RegisterMap( 5, 5, 5, 1280, 4096, 1, "TerMur",		MapRules.HarmfulRestrictions );
 
 			RegisterMap( 0x7F, 0x7F, 0x7F, Map.SectorSize, Map.SectorSize, 1, "Internal", MapRules.Internal );
 
