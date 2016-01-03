b756b71b976da8b331e64c9bc1ee194eee7a8286
Victor Nogueira <felladrin@gmail.com>
2016-01-02 22:06:33 -0300

    Now when you use [P without any argument, it shows your party fellows names and informs who is leading the party.

--- Scripts/Custom/Standalone/Commands/Player/PartyChat.cs
+++ Scripts/Custom/Standalone/Commands/Player/PartyChat.cs
@@ -1,13 +1,17 @@
-//   ___|========================|___   Party Chat Command - Semantic Version: 1.0.0
-//   \  |  Written by Felladrin  |  /   Created at: 2015-12-19 (Felladrin)
-//    > |     December 2015      | <    Updated at: 2015-12-19 (Felladrin)
-//   /__|========================|__\   Description: Sends a message to the party.
+// Party Chat Command v1.0.1
+// Description: Sends a message to the party.
+// Author: Felladrin
+// Started: 2015-12-19
+// Updated: 2016-01-02
 
+using Server;
+using Server.Commands;
 using Server.Engines.PartySystem;
+using System.Collections.Generic;
 
-namespace Server.Commands
+namespace Felladrin.Commands
 {
-    public class PartyChatCommand
+    public static class PartyChatCommand
     {
         public static void Initialize()
         {
@@ -15,24 +19,44 @@ namespace Server.Commands
         }
 
         [Usage("P <text>")]
-        [Description("Sends a message to your party.")]
-        private static void OnCommand(CommandEventArgs e)
+        [Description("Sends a message to your party. If no message is set, lists the party members names.")]
+        static void OnCommand(CommandEventArgs e)
         {
             string text = e.ArgString.Trim();
             Mobile from = e.Mobile;
+            Party p = Party.Get(from);
 
-            if (text.Length == 0 || text.Length > 128)
+            if (p == null)
             {
-                from.SendMessage("Command usage: [p <text>");
+                from.SendLocalizedMessage(3000211); // You are not in a party.
                 return;
             }
 
-            Party p = Party.Get(from);
+            if (text.Length == 0)
+            {
+                List<string> names = new List<string>();
 
-            if (p != null)
-                p.SendPublicMessage(from, text);
+                foreach (PartyMemberInfo pmi in p.Members)
+                {
+                    if (from == pmi.Mobile)
+                        continue;
+                    
+                    names.Add(pmi.Mobile.Name);
+                }
+
+                string leaderInfo = "You are the leader.";
+
+                if (from != p.Leader)
+                {
+                    leaderInfo = string.Format("{0} is the leader.", p.Leader.Name);
+                }
+
+                from.SendMessage("Your have {0} fellow{1} in your party: {2}. {3}", names.Count, (names.Count > 1 ? "s" : ""), string.Join(", ", names), leaderInfo);
+            }
             else
-                from.SendLocalizedMessage(3000211); // You are not in a party.
+            {
+                p.SendPublicMessage(from, text);
+            }
         }
     }
 }
