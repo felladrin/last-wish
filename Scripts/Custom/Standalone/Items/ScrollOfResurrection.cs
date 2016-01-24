e64e52414fe5f68f321c2266a9f8f7ab2a34a12e
Victor Nogueira <felladrin@gmail.com>
2016-01-24 19:03:08 -0300

    Updated Scroll of Resurrection script.

--- Scripts/Custom/Standalone/Items/ScrollOfResurrection.cs
+++ Scripts/Custom/Standalone/Items/ScrollOfResurrection.cs
@@ -1,90 +1,69 @@
-//   ___|========================|___
-//   \  |  Written by Felladrin  |  /
-//    > |       July 2007        | <
-//   /__|========================|__\
+// Scroll Of Resurrection v1.0.1
+// Author: Felladrin
+// Started: 2007-07-07
+// Updated: 2016-01-23
 
+using Server;
+using Server.Items;
 using Server.Mobiles;
 using Server.Targeting;
 
-namespace Server.Items
+namespace Felladrin.Items
 {
     public class ScrollOfResurrection : Item
     {
         [Constructable]
-        public ScrollOfResurrection() : this(1) { }
-
-        [Constructable]
-        public ScrollOfResurrection(int amount) : base(0x227B)
+        public ScrollOfResurrection() : base(0x227B)
         {
             Name = "Scroll of Resurrection";
-            Stackable = false;
-            Weight = 1.0;
-            Amount = amount;
         }
 
         public override void OnDoubleClick(Mobile from)
         {
-            if (IsChildOf(from.Backpack))
-            {
-                from.BeginTarget(2, false, TargetFlags.Beneficial, new TargetCallback(OnTarget));
-                from.SendMessage("Who would you like to resurrect?");
-            }
-            else
+            if (!IsChildOf(from.Backpack))
             {
                 from.SendLocalizedMessage(1042001); // That must be in your pack for you to use it.
-            }
-        }
-
-        public virtual void OnTarget(Mobile from, object obj)
-        {
-            Mobile mob;
-
-            if (obj is Corpse)
-            {
-                Corpse corpse = obj as Corpse;
-                mob = corpse.Owner;
-            }
-            else
-            {
-                mob = obj as Mobile;
-            }
-
-            if (mob == null)
-            {
-                from.SendMessage("This is not a living being!");
                 return;
             }
 
-            if (mob is BaseCreature && mob.IsDeadBondedPet)
-            {
-                BaseCreature bc = mob as BaseCreature;
-                bc.PlaySound(0x214);
-                bc.FixedEffect(0x376A, 10, 16);
-                bc.ResurrectPet();
+            from.SendMessage("Who would you like to resurrect?");
+            from.BeginTarget(2, false, TargetFlags.Beneficial, OnTarget);
+        }
 
-                this.Consume();
-                from.Emote("*recites a scroll of resurrection*");
-                from.SendMessage("The creature was brought back to life.");
+        void OnTarget(Mobile from, object obj)
+        {
+            var mob = obj as Mobile;
+            var corpse = obj as Corpse;
+
+            if (mob == null && corpse == null)
+            {
+                from.SendMessage(33, "You can't resurrect that!");
+                return;
             }
 
-            if (mob is PlayerMobile)
-            {
-                if (mob.Alive)
-                {
-                    from.SendMessage("This creature is not dead!");
-                    return;
-                }
-                else
-                {
-                    mob.PlaySound(0x214);
-                    mob.FixedEffect(0x376A, 10, 16);
-                    mob.Resurrect();
-                }
+            if (corpse != null && corpse.Owner != null)
+                mob = corpse.Owner;
 
-                this.Consume();
-                from.Emote("*recites a scroll of resurrection*");
-                from.SendMessage("This person was brought back to life.");
+            if (mob.IsDeadBondedPet && mob is BaseCreature)
+            {
+                ((BaseCreature)mob).ResurrectPet();
             }
+            else if (mob.Alive)
+            {
+                from.SendMessage(33, "This creature is not dead!");
+                return;
+            }
+            else
+            {
+                mob.Resurrect();
+            }
+
+            mob.PlaySound(0x214);
+            mob.FixedEffect(0x376A, 10, 16);
+
+            Consume();
+            from.Emote("*recites a scroll of resurrection*");
+            from.SendMessage(67, "The creature was brought back to life.");
         }
 
         public ScrollOfResurrection(Serial serial) : base(serial) { }
@@ -92,13 +71,13 @@ namespace Server.Items
         public override void Serialize(GenericWriter writer)
         {
             base.Serialize(writer);
-            writer.Write((int)0);
+            writer.Write(0);
         }
 
         public override void Deserialize(GenericReader reader)
         {
             base.Deserialize(reader);
-            int version = reader.ReadInt();
+            reader.ReadInt();
         }
     }
 }
