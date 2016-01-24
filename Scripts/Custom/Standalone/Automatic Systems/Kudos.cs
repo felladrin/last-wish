9a3c4ced81301ee94b9210dda264c737fc6e4cb1
Victor Nogueira <felladrin@gmail.com>
2016-01-24 19:05:49 -0300

    Updated Kudos script.

--- Scripts/Custom/Standalone/Automatic Systems/Kudos.cs
+++ Scripts/Custom/Standalone/Automatic Systems/Kudos.cs
@@ -1,18 +1,20 @@
-﻿//   ___|========================|___
-//   \  |  Written by Felladrin  |  /   [Kudos - Exchangeable Game Time Reward] - Current version: 1.3 (September 10, 2013)
-//    > |       July 2013        | <
-//   /__|========================|__\   Description: An item given to players for each period of time they stay online.
+﻿// Kudos v1.4.0
+// Author: Felladrin
+// Created at 2013-07-23
+// Updated at 2016-01-18
 
 using System;
+using Server;
 using Server.Mobiles;
 using Server.Network;
 
-namespace Server.Custom.GameTimeReward
+namespace Felladrin.Items
 {
-    public class KudosTimer : Timer
+    public class Kudos : Item
     {
         public static class Config
         {
+            public static bool Enabled = true;                              // Is the kudos auto distribution enabled?
             public static int MinutesOnline = 1;                            // Every X minutes we give kudos to all players online.
             public static bool DropOnBank = true;                           // Should we place the kudos on character's bankbox (true) or backpack (false)?
             public static AccessLevel MaxAccessLevel = AccessLevel.Player;  // Any character with this access and below receives kudos.
@@ -20,55 +22,10 @@ namespace Server.Custom.GameTimeReward
 
         public static void Initialize()
         {
-            new KudosTimer().Start();
+            if (Config.Enabled)
+                new KudosAutoDistributionTimer().Start();
         }
 
-        public KudosTimer() : base(TimeSpan.Zero, TimeSpan.FromMinutes(Config.MinutesOnline))
-        {
-            Priority = TimerPriority.OneMinute;
-        }
-
-        protected override void OnTick()
-        {
-            foreach (NetState state in NetState.Instances)
-            {
-                Mobile m = state.Mobile;
-
-                if (m != null && m is PlayerMobile && m.AccessLevel <= Config.MaxAccessLevel)
-                {
-                    if (Config.DropOnBank && m.BankBox != null)
-                    {
-                        Item kudos = m.BankBox.FindItemByType(typeof(Kudos));
-
-                        if (kudos != null)
-                        {
-                            kudos.Amount++;
-                        }
-                        else
-                        {
-                            m.BankBox.DropItem(new Kudos());
-                        }
-                    }
-                    else if (m.Backpack != null)
-                    {
-                        Item kudos = m.Backpack.FindItemByType(typeof(Kudos));
-
-                        if (kudos != null)
-                        {
-                            kudos.Amount++;
-                        }
-                        else
-                        {
-                            m.Backpack.DropItem(new Kudos());
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    public class Kudos : Item
-    {
         [Constructable]
         public Kudos(int amount)
         {
@@ -96,13 +53,59 @@ namespace Server.Custom.GameTimeReward
         public override void Serialize(GenericWriter writer)
         {
             base.Serialize(writer);
-            writer.Write((int)0);
+            writer.Write(0);
         }
 
         public override void Deserialize(GenericReader reader)
         {
             base.Deserialize(reader);
-            int version = reader.ReadInt();
+            reader.ReadInt();
+        }
+
+        class KudosAutoDistributionTimer : Timer
+        {
+            public KudosAutoDistributionTimer() : base(TimeSpan.Zero, TimeSpan.FromMinutes(Config.MinutesOnline))
+            {
+                Priority = TimerPriority.OneMinute;
+            }
+
+            protected override void OnTick()
+            {
+                foreach (NetState state in NetState.Instances)
+                {
+                    PlayerMobile m = state.Mobile as PlayerMobile;
+
+                    if (m != null && m.AccessLevel <= Config.MaxAccessLevel)
+                    {
+                        if (Config.DropOnBank && m.BankBox != null)
+                        {
+                            Item kudos = m.BankBox.FindItemByType(typeof(Kudos));
+
+                            if (kudos != null)
+                            {
+                                kudos.Amount++;
+                            }
+                            else
+                            {
+                                m.BankBox.DropItem(new Kudos());
+                            }
+                        }
+                        else if (m.Backpack != null)
+                        {
+                            Item kudos = m.Backpack.FindItemByType(typeof(Kudos));
+
+                            if (kudos != null)
+                            {
+                                kudos.Amount++;
+                            }
+                            else
+                            {
+                                m.Backpack.DropItem(new Kudos());
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
