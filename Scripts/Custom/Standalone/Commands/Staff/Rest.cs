491da206c3a06a74f3c78f728124dcf33b6b8c9e
Victor Nogueira <felladrin@gmail.com>
2016-01-24 19:02:52 -0300

    Updated Rest Command script.

--- Scripts/Custom/Standalone/Commands/Staff/Rest.cs
+++ Scripts/Custom/Standalone/Commands/Staff/Rest.cs
@@ -1,41 +1,89 @@
-﻿//   ___|========================|___
-//   \  |  Written by Felladrin  |  /
-//    > |      January 2011      | <
-//   /__|========================|__\
+﻿// Rest Command v1.1.0
+// Author: Felladrin
+// Started: 2011-01-06
+// Updated: 2016-01-21
 
+using Server;
+using Server.Commands;
 using Server.Targeting;
+using Server.Spells.Necromancy;
+using Server.Spells.Fourth;
+using Server.Items;
 
-namespace Server.Commands
+namespace Felladrin.Commands
 {
-    public class Rest
+    public static class Rest
     {
         public static void Initialize()
         {
-            CommandSystem.Register("Rest", AccessLevel.GameMaster, new CommandEventHandler(Restore_OnCommand));
+            CommandSystem.Register("Rest", AccessLevel.Counselor, new CommandEventHandler(OnCommand));
         }
 
         [Usage("Rest")]
-        [Description("Completely restore a character (life, mana, stam, hunger, thirst).")]
-        public static void Restore_OnCommand(CommandEventArgs e)
+        [Description("Completely restores a mobile. It fills hits, mana, stam, hunger, thirst and cure poison, paralysis, wounds and curses. Also resurrects the target if needed.")]
+        static void OnCommand(CommandEventArgs e)
         {
-            e.Mobile.Target = new restTarget();
+            e.Mobile.Target = new RestTarget();
         }
 
-        public class restTarget : Target
+        class RestTarget : Target
         {
-            public restTarget() : base(-1, false, TargetFlags.None) { }
+            public RestTarget() : base(-1, false, TargetFlags.None) { }
 
             protected override void OnTarget(Mobile from, object targeted)
             {
-                if (targeted is Mobile)
+                var m = targeted as Mobile;
+
+                if (m == null)
                 {
-                    Mobile targ = (Mobile)targeted;
-                    targ.Hits = targ.HitsMax;
-                    targ.Mana = targ.ManaMax;
-                    targ.Stam = targ.StamMax;
-                    targ.Hunger = 20;
-                    targ.Thirst = 20;
+                    from.SendMessage("This is not a mobile!");
+                    return;
                 }
+
+                CommandLogging.WriteLine(from, "{0} {1} completely restoring {2}", from.AccessLevel, CommandLogging.Format(from), CommandLogging.Format(m));
+
+                if (!m.Alive)
+                    m.Resurrect();
+
+                m.Hits = m.HitsMax;
+                m.Mana = m.ManaMax;
+                m.Stam = m.StamMax;
+                m.Hunger = 20;
+                m.Thirst = 20;
+                m.Poison = null;
+                m.Paralyzed = false;
+
+                StatMod mod;
+
+                mod = m.GetStatMod("[Magic] Str Offset");
+                if (mod != null && mod.Offset < 0)
+                    m.RemoveStatMod("[Magic] Str Offset");
+
+                mod = m.GetStatMod("[Magic] Dex Offset");
+                if (mod != null && mod.Offset < 0)
+                    m.RemoveStatMod("[Magic] Dex Offset");
+
+                mod = m.GetStatMod("[Magic] Int Offset");
+                if (mod != null && mod.Offset < 0)
+                    m.RemoveStatMod("[Magic] Int Offset");
+
+                EvilOmenSpell.TryEndEffect(m);
+                StrangleSpell.RemoveCurse(m);
+                CorpseSkinSpell.RemoveCurse(m);
+                CurseSpell.RemoveEffect(m);
+                MortalStrike.EndWound(m);
+                BloodOathSpell.RemoveCurse(m);
+                MindRotSpell.ClearMindRotScalar(m);
+
+                BuffInfo.RemoveBuff(m, BuffIcon.Clumsy);
+                BuffInfo.RemoveBuff(m, BuffIcon.FeebleMind);
+                BuffInfo.RemoveBuff(m, BuffIcon.Weaken);
+                BuffInfo.RemoveBuff(m, BuffIcon.Curse);
+                BuffInfo.RemoveBuff(m, BuffIcon.MassCurse);
+                BuffInfo.RemoveBuff(m, BuffIcon.MortalStrike);
+                BuffInfo.RemoveBuff(m, BuffIcon.Mindrot);
+
+                from.SendMessage("{0} has been completely restored!", m.Name);
             }
         }
     }
